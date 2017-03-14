1eca4c6389ce9b2c24ef0cb462afac148518f673
Maarten Ackermans <maarten.ackermans@gmail.com>
2017-03-14 12:49:00 +0100

    fix(jq.run): single-quote command to prevent shell variable evaluation
    fixes #81

--- src/command.js
+++ src/command.js
@@ -6,14 +6,14 @@ const JQ_PATH = path.join(__dirname, '..', 'bin', 'jq')
 const escapeArg = (arg) => {
   arg = arg.toString()
 
-  // Double up all the backslashes and escape the double quote
-  arg = arg.replace(/(\\*)"/g, '$1$1\\"')
+  // Double up all the backslashes and escape the single quote
+  arg = arg.replace(/(\\*)'/g, "$1$1\\'")
 
-  // Escape the backslashes that would escape the double quote that's added
+  // Escape the backslashes that would escape the single quote that's added
   // later to the end of the string
-  arg = arg.replace(/(\\*)$/, '$1$1')
+  arg = arg.replace(/(\\*)$/, `$1$1`)
 
-  arg = '"' + arg + '"'
+  arg = "'" + arg + "'"
   return arg
 }
 
--- test/jq.test.js
+++ test/jq.test.js
@@ -6,13 +6,16 @@ import path from 'path'
 import { run } from '../src/jq'
 
 const PATH_ROOT = path.join(__dirname, '..')
+const PATH_ASTERISK_FIXTURE = path.join(PATH_ROOT, 'src', '*.js')
 const PATH_FIXTURES = path.join('test', 'fixtures')
+
 const PATH_JSON_FIXTURE = path.join(PATH_FIXTURES, '1.json')
 const PATH_JS_FIXTURE = path.join(PATH_FIXTURES, '1.js')
-const PATH_ASTERISK_FIXTURE = path.join(PATH_ROOT, 'src', '*.js')
+const PATH_VARIABLE_JSON_FIXTURE = path.join(PATH_FIXTURES, 'var.json')
 
 const FILTER_VALID = '.repository.type'
 const FILTER_INVALID = 'invalid'
+const FILTER_WITH_VARIABLE = '[ . as $x | .user[] | {"user": ., "site": $x.site} ]'
 
 const ERROR_INVALID_FILTER = /error: invalid/
 const ERROR_INVALID_PATH = 'Invalid path'
@@ -41,5 +44,11 @@ describe('jq core', () => {
     return expect(
       run(FILTER_VALID, PATH_JS_FIXTURE)
     ).to.eventually.be.rejectedWith(Error, ERROR_INVALID_JSON_PATH)
+  })
+
+  it('should not evaluate variables in the shell', () => {
+    return expect(
+      run(FILTER_WITH_VARIABLE, PATH_VARIABLE_JSON_FIXTURE)
+    ).to.eventually.have('user')
   })
 })
